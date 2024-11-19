67fdd4fefbdc6aa92a9c467f69789f45827713da
Caddilac1 <155891049+Caddilac1@users.noreply.github.com>
2024-11-18 21:29:52 -0800

    updated settings.py

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'TodoList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', 
+        conn_max_age=600, 
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
